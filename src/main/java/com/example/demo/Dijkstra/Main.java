88de13877b485dee9f128c99ffd8b9bb3fbc3e4e
yunsoe <yunsoe3105@gmail.com>
2021-11-04 12:17:50 +0800

    Update Main.java

--- src/main/java/com/example/demo/Dijkstra/Main.java
+++ src/main/java/com/example/demo/Dijkstra/Main.java
@@ -1,36 +1,57 @@
-// package com.example.demo.Dijkstra;
+package com.example.demo.Dijkstra;
 
-// public class Main {
-//     public static void main(String[] args) {
-//         Node nodeA = new Node("A");
-//         Node nodeB = new Node("B");
-//         Node nodeC = new Node("C");
-//         Node nodeD = new Node("D"); 
-//         Node nodeE = new Node("E");
-//         Node nodeF = new Node("F");
+public class Main {
+    public void run(ArrayList<) {
+        // Node nodeA = new Node("A");
+        
+        // nodeA.addDestination(nodeB, 10);
+        
+        // Graph graph = new Graph();
 
-//         nodeA.addDestination(nodeB, 10);
-//         nodeA.addDestination(nodeC, 15);
+        // graph.addNode(nodeA);
+        // graph.addNode(nodeB);
+        // graph.addNode(nodeC);
+        // graph.addNode(nodeD);
+        // graph.addNode(nodeE);
+        // graph.addNode(nodeF);
 
-//         nodeB.addDestination(nodeD, 12);
-//         nodeB.addDestination(nodeF, 15);
+        // graph = Dijkstra.calculateShortestPathFromSource(graph, nodeA);
 
-//         nodeC.addDestination(nodeE, 10);
+        // make the location of user the source node
+        Node source = new Node("UserLocation");
 
-//         nodeD.addDestination(nodeE, 2);
-//         nodeD.addDestination(nodeF, 1);
+        // link every other location in the database to the source
+        for () {
+            source.addDestination(, distance);
+        }
 
-//         nodeF.addDestination(nodeE, 5);
 
-//         Graph graph = new Graph();
 
-//         graph.addNode(nodeA);
-//         graph.addNode(nodeB);
-//         graph.addNode(nodeC);
-//         graph.addNode(nodeD);
-//         graph.addNode(nodeE);
-//         graph.addNode(nodeF);
 
-//         graph = Dijkstra.calculateShortestPathFromSource(graph, nodeA);
-//     }
-// }
+    }
+
+    public static double distance(double lat1, double lon1, double lat2, double lon2) {
+
+		// The math module contains a function
+		// named toRadians which converts from
+		// degrees to radians.
+		lon1 = Math.toRadians(lon1);
+		lon2 = Math.toRadians(lon2);
+		lat1 = Math.toRadians(lat1);
+		lat2 = Math.toRadians(lat2);
+
+		// Haversine formula
+		double dlon = lon2 - lon1;
+		double dlat = lat2 - lat1;
+		double a = Math.pow(Math.sin(dlat / 2), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlon / 2), 2);
+
+		double c = 2 * Math.asin(Math.sqrt(a));
+
+		// Radius of earth in kilometers. Use 3956
+		// for miles
+		double r = 6371;
+
+		// calculate the result
+		return (c * r);
+	}
+}
